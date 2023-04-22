a8672b1465165379b458ea9e51c88f8c0bc596cb
Habeebability <awoyemi.habeeb@gmail.com>
2023-04-22 13:34:11 +0100

    added: left side bar

---
+++ components/Sidebar/Left/Tab.vue
@@ -0,0 +1,28 @@
+<template>
+  <NuxtLink
+    to="#"
+    class="flex items-center p-3 w-min hover:bg-gray-200 rounded-full dark:hover:bg-dim-200 text-black dark:text-white"
+    :class="defaultTransition"
+  >
+    <div class="w-6 h-6 text-dark" :class="defaultTransition">
+      <slot name="icon"></slot>
+    </div>
+    <div class="hidden ml-4 text-xl xl:block" :class="labelClasses">
+      <slot name="name"></slot>
+    </div>
+    <!-- <slot /> -->
+  </NuxtLink>
+</template>
+<script setup>
+const { defaultTransition } = useTailwindConfig();
+const props = defineProps({
+  active: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+const labelClasses = computed(() =>
+  props.active ? "font-semibold" : "font-normal"
+);
+</script>
---
+++ components/Sidebar/Left/index.vue
@@ -0,0 +1,88 @@
+<template>
+  <div class="h-screen flex flex-col">
+    <!-- twitter logo -->
+    <div
+      class="p-2 my-2 hover:bg-blue-50 rounded-full w-min dark:hover:bg-white/20"
+      :class="defaultTransition"
+    >
+      <NuxtLink to="/">
+        <div class="w-8 h-8">
+          <LogoTwiter />
+        </div>
+      </NuxtLink>
+    </div>
+
+    <div class="mt-2 space-y-3">
+      <SidebarLeftTab active>
+        <template v-slot:icon>
+          <HomeIcon />
+        </template>
+        <template v-slot:name>Home</template>
+      </SidebarLeftTab>
+      <SidebarLeftTab>
+        <template v-slot:icon>
+          <HashtagIcon />
+        </template>
+        <template v-slot:name>Explore</template>
+      </SidebarLeftTab>
+      <SidebarLeftTab>
+        <template v-slot:icon>
+          <BellIcon />
+        </template>
+        <template v-slot:name>Notifications</template>
+      </SidebarLeftTab>
+      <SidebarLeftTab>
+        <template v-slot:icon>
+          <InboxIcon />
+        </template>
+        <template v-slot:name>Messages</template>
+      </SidebarLeftTab>
+      <SidebarLeftTab>
+        <template v-slot:icon>
+          <BookmarkIcon />
+        </template>
+        <template v-slot:name>Bookmarks</template>
+      </SidebarLeftTab>
+      <SidebarLeftTab>
+        <template v-slot:icon>
+          <DocumentTextIcon />
+        </template>
+        <template v-slot:name>Lists</template>
+      </SidebarLeftTab>
+      <SidebarLeftTab>
+        <template v-slot:icon>
+          <UserIcon />
+        </template>
+        <template v-slot:name>Profile</template>
+      </SidebarLeftTab>
+      <SidebarLeftTab>
+        <template v-slot:icon>
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 28 28" width="28" height="28">
+            <circle cx="14" cy="14" r="12" fill="none" stroke="black" />
+            <circle cx="7" cy="14" r="2" fill="black" />
+            <circle cx="14" cy="14" r="2" fill="black" />
+            <circle cx="21" cy="14" r="2" fill="black" />
+          </svg>
+          <!-- <DotsHorizontalIcon /> -->
+          <!-- <DotsCircleHorizontalIcon /> -->
+        </template>
+        <template v-slot:name>More</template>
+      </SidebarLeftTab>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { HomeIcon } from "@heroicons/vue/20/solid";
+import {
+  HashtagIcon,
+  BellIcon,
+  InboxIcon,
+  BookmarkIcon,
+  DocumentTextIcon,
+  UserIcon,
+} from "@heroicons/vue/24/outline";
+
+const { defaultTransition } = useTailwindConfig();
+</script>
+
